c4b3d25b78a965e7d1b18e772276d0257e85a50e
YuliyaGor <d_w@ukr.net>
2015-04-08 10:56:24 +0300

    Create MathInterface.java

---
+++ MathInterface.java
@@ -0,0 +1,17 @@
+package poliMorf;
+
+public interface MathInterface {
+	
+	public int multi(int multA, int multB);
+
+	public int divide(int divA, int divB);
+
+	public int plus(int summA, int summB);
+
+	public int minus(int minA, int minB);
+	
+	public void maximum(Integer...param);
+	
+	public void minimum(Integer...param);
+
+}
